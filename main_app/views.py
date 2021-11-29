3947a27922bfec457b077893ca74bc513dbb7453
ralrubayee <ralrubayee@gmail.com>
2021-11-29 16:02:01 -0500

    add stylesheets

--- main_app/views.py
+++ main_app/views.py
@@ -6,5 +6,7 @@ from django.http import HttpResponse
 def home(request):
   return HttpResponse('<h1>Hello</h1>')
 
+def about(request):
+  return render(request, 'about.html')
 
 # Create your views here.
