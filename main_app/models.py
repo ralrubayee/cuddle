66c1c7cbd399c80979949e0495b731845811d635
ralrubayee <ralrubayee@gmail.com>
2021-11-30 10:08:28 -0500

    linked the database

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Tips(models.Model):
+  tips_catgory = models.CharField(max_length=100)
+  title = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  
