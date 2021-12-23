0433b07889fbbb8ba86ea0c718f60c6f4f6ac1d9
ralrubayee <ralrubayee@gmail.com>
2021-12-22 21:23:55 -0500

    Config deployment

--- cuddle/settings.py
+++ cuddle/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x_+zt@)swv)ca3n72!d_*po@xop#h%njd$mzf$j^*72!v-r*#x'
-SECRET_KEY = env('ot+hbuoqvp#seuy0!f^&f%tlrg)$by2w%uh%z_uhht7om0olnw')
+# SECRET_KEY = 'django-insecure-x_+zt@)swv)ca3n72!d_*po@xop#h%njd$mzf$j^*72!v-r*#x'
+SECRET_KEY = env('SECRET_KEY')
 DEBUG_PROPAGATE_EXCEPTIONS = True
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
